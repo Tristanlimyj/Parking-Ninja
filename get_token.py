3f0df8a566d682b698301b87a89a18438382e4a9
tristanlimyj <tristanlimyj@gmail.com>
2020-12-25 00:40:51 +0800

    creating the first file

---
+++ get_token.py
@@ -0,0 +1,14 @@
+# Get the AccessKey from the Env Var
+
+# Post Response
+
+# Await for the return of the Token
+
+# Checks
+    ''' 
+        Get Ensure that it is unique and not the previous token
+    '''
+
+# Add to DB
+
+#
